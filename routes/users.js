6ddf121d96c76cfe985bd08a694e16e5f85c7059
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2022-03-11 12:25:56 -0500

    create initial user model

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../model/db');
+const userModel = require('../model/user');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+
+    let result = await userModel.getUsers();
+    res.render('users', result);
 });
+router.get('/:userId', async function(req, res, next) {
+
+    let userId = parseInt(req.params.userId.trim());
+    let result = await userModel.getUser(userId);
+    //console.log(result);
+    res.send(result);
+});
+
 
 module.exports = router;
